67b3936ee8dad0001a7f9fd7591824899299302f
Assel-Aljazwe <157559559+Assel-Aljazwe@users.noreply.github.com>
2024-01-24 16:09:09 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function sum(a) {
-    var sum = a[0];
+    var sum = 0; //previously a[0] which is a logical error as it counts the first element twice.
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
